b8589b73875df07f5e2181fd05bfbe4be5a65abd
poojithamotha <47564996+poojithamotha@users.noreply.github.com>
2025-10-17 08:38:19 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "web" {
   name  = "tf-nginx"
-  image = docker_image.nginx.image_id # ✅ fixed here
+  image = docker_image.nginx.image_id 
 
   ports {
     internal = 80
